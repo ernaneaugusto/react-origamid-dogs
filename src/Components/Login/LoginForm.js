f6c76a39f65aac07ce32127c7416598a0a6ed19a
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-14 20:35:28 -0300

    validacao do form de login com hook useForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import API_URLS from "../../config/api/apiUrls";
 import Input from "./../Forms/Input";
 import Button from "./../Forms/Button";
+import useForm from "./../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log("###", [username, password]);
-
-    fetch(API_URLS.jwtAuth, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((res) => {
-        console.log("res", res);
-        return res.json();
+    if (username.validate() && password.validate()) {
+      fetch(API_URLS.jwtAuth, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log("### json", json);
-      });
+        .then((res) => {
+          console.log("res", res);
+          return res.json();
+        })
+        .then((json) => {
+          console.log("### json", json);
+        });
+    }
   }
 
   return (
@@ -34,8 +34,8 @@ const LoginForm = () => {
       <h1>Login</h1>
 
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
     </section>
