c528e6d0805eb20ef0bc6fe1301ef17178ea4f8f
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-14 20:31:41 -0300

    criado hook useForm

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const initialState = "";
+
+const types = {
+  email: {
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um e-mail válido. Ex: seuemail@email.com",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = useState(initialState);
+  const [error, setError] = useState(null);
+
+  const onChange = (event) => {
+    const value = event.target.value;
+    if(error) validate(value);
+    setValue(value);
+  };
+
+  const validate = (value) => {
+    // para campos com preenchimento sem validade e opcionais
+    if (type === false) return true;
+
+    if (value.length === 0) {
+      setError("Preencha esse campo.");
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  };
+
+  return {
+    value,
+    setValue,
+    onChange,
+    onBlur: () => validate(value),
+    error,
+    validate: () => validate(value),
+  };
+};
+
+export default useForm;
