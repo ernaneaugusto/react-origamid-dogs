d6d09b0e0baed3c2cb2b709e276ce1153d5e80fc
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-14 12:35:17 -0300

    configuracoes e componentes iniciais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import Api from './api/Api';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.scss";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
-    <Api />
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
