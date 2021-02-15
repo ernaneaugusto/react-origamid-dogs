40ea86d1c818fa2e8f56688de342f944dac9a174
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-15 17:03:17 -0300

    navegacao com rotas protegidas ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import ProtectedRoute from "./Components/Shared/ProtectedRoute";
+import User from "./Components/User/User";
+import UserStats from "./Components/User/UserStats";
 // deve ser utilizado para envolver toda parte da aplicacao que vai compartilhar um contexto
 import { UserStorage } from "./Contexts/UserContext";
 
@@ -17,7 +20,9 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
+            <ProtectedRoute path="estatisticas/*" element={<UserStats />} />
           </Routes>
           <Footer />
         </UserStorage>
