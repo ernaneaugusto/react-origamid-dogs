7bc5d6e77ec292dd9af36f4beb6451929fa7b008
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-15 12:09:42 -0300

    novos estilos de animate

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -23,7 +23,7 @@ const LoginForm = () => {
   };
 
   return (
-    <section className="anime-left">
+    <section className="anime-bottom">
       <h1 className="title">Login</h1>
 
       <form onSubmit={handleSubmit} className={styles.form}>
